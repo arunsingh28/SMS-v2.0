108afc264b69917c50edf60f161dc043ce52a29a
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-23 18:27:48 +0530

    connect to DB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,21 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import config from '../config/config'
 import logging from '../config/logger'
 
-
+// init express variable to app ==========
 const app = express()
 
 
 const NAMESPACE = 'server'
 
-// logger
+
+// body parser ===========================
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// logger ===================================
 app.use((req, res, next) => {
     logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`)
     res.on('finish', () => {
@@ -18,31 +25,21 @@ app.use((req, res, next) => {
 })
 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-
-
 // routes ======================================
-/** 
- * import route file from controller
-@param route file
-
- */
-
 import loginRoutes from './routes/login'
 
 app.use('/api/login', loginRoutes)
 
 
 
-
-// error handling
+// error handling ===============================
 app.use((req, res, next) => {
     const error = new Error('Page not found');
     return res.json({ message: error.message, statusCode: 404 }).status(404);
 })
 
+
+// server start =================================
 app.listen(config.server.port, () => {
     console.log(`Server started on port ${config.server.port}`);
 })

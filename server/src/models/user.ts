f43ca41bef0039df1a0b4eae28175cddce14bff3
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-23 23:30:00 +0530

    shield

---
+++ server/src/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const user = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "please provide a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "please provide a password"],
+        minlength: 6,
+        select: false
+    },
+    profile: {
+        type: String,
+        required: true
+    }
+})
+
+
+export default user

65e59c04dd672d4564c3e469a373c04ef7049739
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-24 17:51:13 +0530

    aws module done

--- server/src/utils/uploader.ts
+++ server/src/utils/uploader.ts
@@ -1,14 +1,14 @@
 import multerS3 from 'multer-s3'
 import multer from 'multer'
-import AWS from 'aws-sdk'
+import AWS, { CodeBuild } from 'aws-sdk'
 
 declare var process: {
     env: {
         AWS_ACESS_KEY: string,
-        AWS_SECRET_KEY: string
+        AWS_SECRET_KEY: string,
+        AWS_BUCKET_NAME: string
     }
 }
-
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_SECRET_KEY,
@@ -16,7 +16,17 @@ const s3 = new AWS.S3({
 })
 
 export default function () {
-    const upload = multer({
-
+    multer({
+        storage: multerS3({
+            s3,
+            bucket: process.env.AWS_BUCKET_NAME,
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                cb(null, { fielName: file.fieldname })
+            },
+            key: function (req, file, cb) {
+                cb(null, Date.now().toString() + '-' + file.originalname)
+            }
+        })
     })
 }

bcd54d7d44d599b93da951c383d9bc61a6c3c7d5
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-23 19:23:36 +0530

    info page

---
+++ client/components/Info.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SubMenu from "./partials/submenu";
+const Info = () => {
+  return (
+    <div className="card bg-gray-200">
+      <div className="flex">
+        <img
+          src="https://avatars.githubusercontent.com/u/48112517?v=4"
+          alt="profile"
+          className="max-h-48 bo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Info;

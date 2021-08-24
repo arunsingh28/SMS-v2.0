7596cff61666a4def9d372324962abaead41e456
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-25 00:16:30 +0530

    ADJUST: port

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost'
-const SERVER_PORT = process.env.SERVER_PORT || 8080
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
+const SERVER_PORT = process.env.PORT || 8080;
 
 const server = {
-    hostname : SERVER_HOSTNAME,
-    port: SERVER_PORT
-}
+  hostname: SERVER_HOSTNAME,
+  port: SERVER_PORT,
+};
 
-
-
-export default server
+export default server;

b6cd6c45cdedfc98ba00be5cae4e3bc4da1f7e49
Arun Singh <arunsingh28aug.as@gmail.com>
2021-09-19 06:44:09 +0530

    ****

---
+++ test/src/components/Sidebar_data.js
@@ -0,0 +1,47 @@
+export const Sidebar = [{
+        title: "overview",
+        path: "/overview",
+        icon: "as",
+        iconClosed: "close",
+        iconOpendd: "open",
+        subNav: [{
+                title: "user",
+                path: "/user",
+                icon: "user",
+            },
+            {
+                title: "user-1",
+                path: "/user-1",
+                icon: "user-1",
+            },
+        ],
+    },
+    {
+        title: "Reports",
+        path: "/reports",
+        icon: "report",
+    },
+    {
+        title: "overview",
+        path: "/overview",
+        icon: "as",
+    },
+    {
+        title: "overview",
+        path: "/overview",
+        icon: "as",
+        iconClosed: "close",
+        iconOpendd: "open",
+        subNav: [{
+                title: "user",
+                path: "/user",
+                icon: "user",
+            },
+            {
+                title: "user-1",
+                path: "/user-1",
+                icon: "user-1",
+            },
+        ],
+    },
+];

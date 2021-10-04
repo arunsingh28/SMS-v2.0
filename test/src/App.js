907e538efa004858c1923b1d5be3d8b1bb749f0d
Arun Singh <arunsingh28aug.as@gmail.com>
2021-10-04 18:22:28 +0530

    auth test

--- test/src/App.js
+++ test/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
-
+import Protected from "./components/Protected";
+import { Route, Switch } from "react-router-dom";
 const App = () => {
-  return <Sidebar />;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <h1>Home page</h1>
+      </Route>
+      <Route component={Protected} exact path="/app" />
+    </Switch>
+  );
 };
 
 export default App;
---
+++ test/src/components/Protected.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Protected = () => {
+  return <div>Private Route</div>;
+};
+
+export default Protected;

3da072f5e63368b38b27399e685f7f2de94629fc
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-24 09:23:51 -0700

    fix merging bug

--- server/config/allowedOrigin.ts
+++ server/config/allowedOrigin.ts
@@ -2,7 +2,7 @@ const allowedOrigin = [
     'http://localhost:3000',
     'https://domain.com',
     "http://localhost:8080",
-    "https://grey-moon-924431.postman.co"
+    "https://grey-moon-924431.postman.co",
     "https://www.youtube.com"
 ]
 
--- server/config/corsOption.ts
+++ server/config/corsOption.ts
@@ -4,15 +4,13 @@ const corsOption = {
     origin: (origin: any, callback: any) => {
         console.log('ORIGIN______', origin)
         // remove !origin it just for dev otherwise it allow all the other origin to access the route
-        if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
-            console.log(origin, '\n')
-            if (allowedOrigin.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        optionSuccessStatus: 200,
+        if (allowedOrigin.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionSuccessStatus: 200,
 }
 
 export default corsOption;

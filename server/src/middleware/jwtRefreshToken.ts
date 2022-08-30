d822bcd46938769acb1e1988f15a304e388306b9
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-30 09:12:50 +0530

    jwt-verify process

--- server/src/middleware/jwtRefreshToken.ts
+++ server/src/middleware/jwtRefreshToken.ts
@@ -1,4 +1,4 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import _user, { UserDocument } from '../models/user.model'
 import { Response, Request, NextFunction } from 'express'
 
@@ -17,40 +17,43 @@ interface JwtPayload {
 }
 export default async function refreshToken(req: Request, res: Response, next: NextFunction) {
     const refresh_token = req.cookies?.jwt
-    console.log('RT:-----', refresh_token)
     if (!refresh_token) return res.sendStatus(401)
-    // match token in DB
+    // find token in DB
     const foundUser = await _user.findOne({ refresh_token }).exec()
-    console.log('User from DB -----', foundUser)
     // res.clearCookie('jwt', {
     //     httpOnly: true,
     //     // sameSite: 'none',
     //     // secure: true,
     // })
+
     if (!foundUser) return res.sendStatus(403)  //Forbidden
+
     // verify token
     try {
-        const decoded: JwtPayload = jwt.verify(refresh_token, process.env.JWT_SECRET_KEY2)
-        console.log('decoded', decoded)
-        // match token in DB
-        if (foundUser.email !== decoded.id) return res.sendStatus(403) //Forbidden
-        // create new access token
+        jwt.verify(refresh_token, process.env.JWT_SECRET_KEY2, (err: any, decoded: any) => {
+            // match token in DB
+            if (err || foundUser.refresh_token !== decoded.id) return res.sendStatus(403) // Forbiden
+            // create new access token
+            const accessToken = jwt.sign({ id: decoded.id }, process.env.JWT_SECRET_KEY1, {
+                expiresIn: process.env.JWT_EXPIRE_TIME
+            })
+            res.cookie('jwt', {
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000,
+                // sameSite: 'none',
+                // secure: true
+            })
+        })
+
 
         // send new access token to client
 
     } catch (error) {
         console.log('error', error)
+        // forbidden
         return res.sendStatus(403)
     }
-
-    // // forbidden
-    // else return res.sendStatus(403)
 }
 
 
 
-// jwt.verify(clientRefreshToken, process.env.JWT_SECRET_KEY2, (err: any, decoded: any) => {
-//     if (err || refreshToken?.id !== decoded.id) return res.sendStatus(403)
-//     const accessToken = jwt.sign({ id: decoded.id }, process.env.JWT_SECRET_KEY1, {
-//         expiresIn: process.env.JWT_EXPIRE_TIME
-//     })

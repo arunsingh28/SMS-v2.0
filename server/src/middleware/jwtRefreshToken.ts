3b10b70fbbce35fba300d7bb8118c709e4036c1e
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-30 09:35:38 +0530

    refresh module done

--- server/src/middleware/jwtRefreshToken.ts
+++ server/src/middleware/jwtRefreshToken.ts
@@ -20,47 +20,41 @@ export default async function refreshToken(req: Request, res: Response, next: Ne
     if (!refresh_token) return res.sendStatus(401)
     // find token in DB
     const foundUser = await _user.findOne({ refresh_token }).exec()
-
     // delete previous cookie //
-
-    // res.clearCookie('jwt', {
-    //     httpOnly: true,
-    //     // sameSite: 'none',
-    //     // secure: true,
-    // })
-
+    res.clearCookie('jwt', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    })
     if (!foundUser) return res.sendStatus(403)  //Forbidden
-
     // verify token
     try {
-        jwt.verify(refresh_token, process.env.JWT_SECRET_KEY2, (err: any, decoded: any) => {
+        jwt.verify(refresh_token, process.env.JWT_SECRET_KEY2, (err: any, decoded: JwtPayload | any) => {
             // match token in DB
-            if (err || foundUser.refresh_token !== decoded.id) return res.sendStatus(403) // Forbiden
+            if (err || foundUser.email !== decoded.id) return res.sendStatus(403) // Forbiden
             // create new access token
             const accessToken = jwt.sign({ id: decoded.id }, process.env.JWT_SECRET_KEY1, {
                 expiresIn: process.env.JWT_EXPIRE_TIME
             })
+            // create new refersh token
             const newRefreshToken = jwt.sign({ id: foundUser.email },
                 process.env.JWT_SECRET_KEY2,
                 { expiresIn: process.env.JWT_REFRESH_EXPIRE_TIME })
-
+            // save new refersh token to DB 
+            foundUser.refresh_token = newRefreshToken
+            foundUser.save()
+            // create cookie with refresh token
             res.cookie('jwt', newRefreshToken, {
                 httpOnly: true,
                 maxAge: 24 * 60 * 60 * 1000,
                 // sameSite: 'none',
                 // secure: true
             })
+            // send new access token to client
+            res.status(200).json({ accessToken })
         })
-
-
-        // send new access token to client
-
     } catch (error) {
         console.log('error', error)
-        // forbidden
-        return res.sendStatus(403)
+        return res.sendStatus(403) //// forbidden
     }
 }
-
-
-

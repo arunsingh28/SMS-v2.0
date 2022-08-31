38151859c26f5531733c6ebcad026bfc06f2173e
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-31 20:18:54 +0530

    disable the cors

--- server/config/corsOption.ts
+++ server/config/corsOption.ts
@@ -3,7 +3,7 @@ import allowedOrigin from "./allowedOrigin";
 const corsOption = {
     origin: (origin: any, callback: any) => {
         // remove !origin it just for dev otherwise it allow all the other origin to access the route
-        if (allowedOrigin.indexOf(origin) !== -1) {
+        if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             const err = new Error('Not allowed by CORS')

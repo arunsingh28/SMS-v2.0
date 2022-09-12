6e0d6f24b9278387a40bb3795c2f5696d2680d6f
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-09-12 21:36:59 +0530

    UPDATE credentials headres [#13]

--- server/src/middleware/credentials.ts
+++ server/src/middleware/credentials.ts
@@ -4,11 +4,8 @@ import { Request, Response, NextFunction } from "express";
 
 const credentials = (req: Request, res: Response, next: NextFunction) => {
     const origin: any = req.headers.origin;
-
-    // if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
-
     if (allowedOrigin.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
     }
     next()
 }
